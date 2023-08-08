456f5c19d03a7f515ff498072cfc5e3ed86920e4
kimjaesung <aamoos@naver.com>
2023-08-08 14:21:32 +0900

    todo input

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,15 +1,14 @@
 <template>
-  <div>
-    header
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
+<!-- scoped 이 style안에서만 존재하는 style 태그 -->
+<style scoped>
+h1{
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
