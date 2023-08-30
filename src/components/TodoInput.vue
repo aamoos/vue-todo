1f14d8d433e70c35e01aa59fe8d14dc9138242f0
aamoos <aamoos@naver.com>
2023-08-30 23:36:12 +0900

    [리팩토링] 속성 메서드의 축약 특징 리팩토링

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -22,7 +22,7 @@
 import Modal from './common/ModalView.vue'
 
 export default {
-  data: function(){
+  data(){
     return{
       newTodoItem: "",
       showModal: false
@@ -30,7 +30,7 @@ export default {
   },
 
   methods: {
-    addTodo: function(){
+    addTodo(){
       if(this.newTodoItem !== ''){
         this.$emit('addTodoItem', this.newTodoItem);
         this.clearInput();
@@ -38,7 +38,7 @@ export default {
         this.showModal = !this.showModal;
       }
     },
-    clearInput: function(){
+    clearInput(){
       this.newTodoItem = '';
     }
   },
