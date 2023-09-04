50b30636db18e8eaccdbcb2a034f95c0c35d29cc
aamoos <aamoos@naver.com>
2023-09-04 23:15:53 +0900

    [리팩토링] state 속성 적용

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
-import Vuex form 'vuex'
+import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
-export const store = Vuex.store({
-    //
+const storage = {
+    fetch(){
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                }
+            }
+        }
+        return arr;
+    }
+}
+
+export const store = new Vuex.Store({
+    state: {
+        todoItems: storage.fetch()
+    }
 });
