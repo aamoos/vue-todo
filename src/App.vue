baada55aeceebb80dffc1971f65da3c59bfc72d1
aamoos <aamoos@naver.com>
2023-09-05 22:20:37 +0900

    props, emit -> vuex로 리팩토링

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
-    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
+    <TodoInput></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:toggleItem="toggleOneItem"></TodoList>
     <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
@@ -20,36 +20,18 @@ export default {
     }
   },
   methods: {
-    addOneItem(todoItem){
-      const obj = { completed: false, item: todoItem };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      this.todoItems.push(obj);
-    },
-    removeOneItem(todoItem, index){
-      localStorage.removeItem(todoItem.item);
-      this.todoItems.splice(index, 1);
-    },
 
-    toggleOneItem(todoItem, index){
-      console.log(todoItem.item)
-      this.todoItems[index].completed = !this.todoItems[index].completed;
-      //로컬 스토리지의 데이터를 갱신
-      localStorage.removeItem(todoItem.item);
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-    },
-    clearAllItems(){
-      localStorage.clear();
-      this.todoItems = [];
-    }
-  },
-  created() {
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
+    // toggleOneItem(todoItem, index){
+    //   console.log(todoItem.item)
+    //   this.todoItems[index].completed = !this.todoItems[index].completed;
+    //   //로컬 스토리지의 데이터를 갱신
+    //   localStorage.removeItem(todoItem.item);
+    //   localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    // },
+    // clearAllItems(){
+    //   localStorage.clear();
+    //   this.todoItems = [];
+    // }
   },
 
   components: {
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -15,10 +15,9 @@
 
 <script>
 export default {
-  props: ['propsdata'],
   methods: {
     removeTodo(todoItem, index){
-      this.$emit('removeItem', todoItem, index);
+      this.$store.commit('removeOneItem', {todoItem, index});
     },
 
     toggleComplete(todoItem, index){
