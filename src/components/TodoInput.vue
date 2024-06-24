edc465867b0ff59843236c97773ddc121776d9ca
김재성 <aamoos@openmate.co.kr>
2024-06-24 10:38:03 +0900

    [퀴즈] slot 퀴즈 풀이 및 모달 컴포넌트 기능 구현

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -6,12 +6,18 @@
       <i class="fas fa-plus addBtn"></i>
     </span>
     <Modal v-if="showModal" @close="showModal = false">
-    <!--
+      <!--
       you can use custom content here to overwrite
       default content
     -->
-    <h3 slot="header">custom header</h3>
-  </Modal>
+      <h3 slot="header">
+        경고!
+        <i class="closeModalBtn fas fa-times" @click="showModal = false"></i>
+      </h3>
+      <div slot="body">
+        아무것도 입력하지 않으셨습니다.
+      </div>
+    </Modal>
 
   </div>
 </template>
@@ -69,5 +75,8 @@ input:focus {
 .addBtn {
   color: white;
   vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
 }
 </style>
--- src/components/common/Modal.vue
+++ src/components/common/Modal.vue
@@ -17,16 +17,6 @@
                   default body
                 </slot>
               </div>
-
-              <!-- 모달 푸터-->
-              <div class="modal-footer">
-                <slot name="footer">
-                  default footer
-                  <button class="modal-default-button" @click="$emit('close')">
-                    OK
-                  </button>
-                </slot>
-              </div>
             </div>
           </div>
         </div>
